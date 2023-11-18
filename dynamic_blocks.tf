a81ed6ece3f2053d5078ab80548e78f4d4d527a9
TuranCyberHub <93497641+turancyberhub@users.noreply.github.com>
2023-11-18 12:10:14 -0500

    Update dynamic_blocks.tf

--- dynamic_blocks.tf
+++ dynamic_blocks.tf
@@ -5,49 +5,54 @@ resource "aws_vpc" "main" {
     }
 }
 
+variable "ingress_rules" {
+    type = list(object({
+        description = string
+        from_port = number
+        to_port = number
+        protocol = string
+        cidr_blocks = list(string)
+    }))
+
+    default = [
+        {
+            description = "allow ssh from my home internet"
+            from_port = 22
+            to_port = 22
+            protocol = tcp
+            cidr_blocks = ["192.168.32.24/30"]
+        },
+        {
+            description = "allow application from load balancer"
+            from_port = 80
+            to_port = 80
+            protocol = tcp
+            cidr_blocks = ["0.0.0.0/0"] 
+        },
+        {
+            description = "allow application 443 from load balancer"
+            from_port = 443
+            to_port = 443
+            protocol = tcp
+            cidr_blocks = ["0.0.0.0/0"] 
+        }
+    ]
+}
+
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-  }
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 8888
-    to_port          = 8888
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+        description = ingress.value
+        from_port   = ingress.value
+        to_port     = ingress.value
+        protocol    = ingress.value
+        cidr_blocks = ingress.value
+    }
   }
 
   egress {
