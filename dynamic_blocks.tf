1bf490b8d645b6b3c8271728427eef2c149b6333
TuranCyberHub <93497641+turancyberhub@users.noreply.github.com>
2023-11-18 12:15:16 -0500

    Update dynamic_blocks.tf

--- dynamic_blocks.tf
+++ dynamic_blocks.tf
@@ -1,41 +1,52 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "sg_vpc"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "sg_vpc"
+  }
 }
-variable "ingress_rules" {
-    type = list(object({
-        description = string
-        from_port = number
-        to_port = number
-        protocol = string
-        cidr_blocks = list(string)
-    }))
 
-    default = [
-        {
-            description = "allow ssh from my home internet"
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-            cidr_blocks = ["192.168.32.24/30"]
-        },
-        {
-            description = "allow application from load balancer"
-            from_port = 80
-            to_port = 80
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"] 
-        },
-        {
-            description = "allow application 443 from load balancer"
-            from_port = 443
-            to_port = 443
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"] 
-        }
-    ]
+variable "ingress_rule_ports" {
+  type    = list(any)
+  default = ["80", "443", "8080", "8888", "22"]
+}
+
+variable "ingress_rule_cidr_blocks" {
+  type    = list(any)
+  default = ["0.0.0.0/0", "192.168.1.1/32", "192.168.1.0/32"]
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+
+  default = [
+    {
+      description = "allow ssh from my home internet"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["192.168.32.24/30"]
+    },
+    {
+      description = "allow application from load balancer"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "allow application 443 from load balancer"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
 
 resource "aws_security_group" "allow_tls" {
@@ -46,19 +57,19 @@ resource "aws_security_group" "allow_tls" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-        description = ingress.value
-        from_port   = ingress.value
-        to_port     = ingress.value
-        protocol    = ingress.value
-        cidr_blocks = ingress.value
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
     }
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
