f26839f964baab39fb3bbe63c3a22035e5d8ffd0
TuranCyberHub <93497641+turancyberhub@users.noreply.github.com>
2023-11-19 10:19:20 -0500

    Update homework3.tf

--- homework3.tf
+++ homework3.tf
@@ -20,6 +20,67 @@ resource "aws_route53_record" "api" {
 }
 
 
+
+#Solution 1:
+
+
+resource "aws_route53_zone" "example" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.example.zone_id
+  name    = "www.example.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["192.0.2.1"]
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = aws_route53_zone.example.zone_id
+  name    = "api.example.com"
+  type    = "A"
+  ttl     = 300
+  records = ["192.0.2.2"]
+}
+
+resource "aws_route53_record" "example_records" {
+    dynamic "record" {
+        for_each = var.dns_records
+        content {
+            zone_id = aws_route53_zone.example.zone_id
+            name = record.value.name
+            type = record.value.type
+            ttl = record.value.ttl
+            records = record.value.records
+        }
+    }
+}
+
+variable "dns_records" {
+    type = list(object({
+        name = string
+        type = string
+        ttl  = number
+        records = list(sting)
+    }))
+    default = [
+        {
+          name = "www.example.com"
+          type    = "A"
+          ttl     = 300
+          records = ["192.0.2.1"]
+        },
+        {
+          name    = "api.example.com"
+          type    = "A"
+          ttl     = 300
+          records = ["192.0.2.2", "192.168.2.3"]
+        }
+    ]
+}
+
+
 ##############################################################################
 # Convert below code to dynamic block, instead of creating 4 resource blocks for subnets, create one dynamic block to create all subnets
   
